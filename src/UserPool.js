d06f94d5b751d0ff5fcc78d06b2678cee88fd3a6
meryemmetav <meyeme.ennajibi@metave.us>
2023-11-03 13:20:31 +0100

    cognito update

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: "us-east-1_rDmxNzFmY",
-    ClientId: "3nliaben0l30vi36udq2le0jop"
+    UserPoolId: "us-east-1_vPTmou7LQ",
+    ClientId: "cm9u685btahag6euug6d92284"
 }
 
 export default new CognitoUserPool(poolData);
